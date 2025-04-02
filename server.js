0387df07768d17064fba3965d9bd95727be92111
BunyodL <blutfulloyev@bk.ru>
2025-04-03 04:29:06 +0500

    start the first server, add routing, fix counter and add custom favicon

---
+++ server.js
@@ -0,0 +1,58 @@
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+const url = require("url");
+
+let requestCount = 0;
+
+// Location of my favicon in the filesystem.
+const FAVICON = path.join(__dirname, "public", "favicon.ico");
+
+const server = http.createServer((req, res) => {
+    const pathname = url.parse(req.url).pathname;
+
+    if (req.method === "GET" && pathname === "/favicon.ico") {
+        // MIME type of the favicon.
+        //
+        // .ico = 'image/x-icon' or 'image/vnd.microsoft.icon'
+        // .png = 'image/png'
+        // .jpg = 'image/jpeg'
+        // .jpeg = 'image/jpeg'
+        res.setHeader("Content-Type", "image/x-icon");
+
+        // Serve your favicon and finish response.
+        //
+        // You don't need to call `.end()` yourself because
+        // `pipe` will do it automatically.
+        fs.createReadStream(FAVICON).pipe(res);
+        return;
+    }
+
+    switch (req.url) {
+        case "/me": {
+            res.write("Me");
+            break;
+        }
+        case "/about": {
+            res.write("About");
+            break;
+        }
+        case "/": {
+            res.write("Main page");
+            break;
+        }
+        default:
+            res.write("404 not found");
+    }
+
+    // Fix the counter from being updated twice
+    if (!req.url.startsWith("/favicon")) {
+        requestCount++;
+    }
+
+    res.write("Node js server " + requestCount);
+
+    res.end();
+});
+
+server.listen(3003);
